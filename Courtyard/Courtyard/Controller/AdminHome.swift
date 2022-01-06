aab21ee3afe18ee06c1d5cfebd7afcbd31a6b462
Abdullah Bajaman <ambajaman@gmail.com>
2022-01-06 09:50:55 +0300

    Update Admin view and controller

--- Courtyard/Courtyard/Controller/AdminHome.swift
+++ Courtyard/Courtyard/Controller/AdminHome.swift
@@ -9,17 +9,36 @@ import UIKit
 
 class AdminHome: UIViewController {
 
+    var orders: [Order] = []
+    var images: [UIImage?] = []
+    
     @IBOutlet weak var serviceCollection: UICollectionView!
+    @IBOutlet weak var ordersTV: UITableView!
     
     @IBOutlet weak var fromDP: UIDatePicker!
     @IBOutlet weak var toDP: UIDatePicker!
-    @IBOutlet weak var ordersTV: UITableView!
     @IBOutlet weak var totalLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let image1 = UIImage(named: "cortyard")
+        let image2 = UIImage(named: "roof of house")
+        let image3 = UIImage(named: "stairs")
+        images = [image1, image2, image3]
+        
+        serviceCollection.delegate = self
+        serviceCollection.dataSource = self
+        
+        ordersTV.delegate = self
+        ordersTV.dataSource = self
+        
+        Admin.shared.getAllOrders { orders in
+            self.orders = orders
+            self.ordersTV.reloadData()
+        }
     }
     
 
@@ -44,12 +63,17 @@ extension AdminHome: UITableViewDelegate {
 }
 extension AdminHome: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        orders.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "orderCell", for: indexPath) as! OrderTVCell
         
+        let order = orders[indexPath.row]
+//        cell.districLbl =
+        cell.userIDLbl.text = order.userId!.documentID
+        cell.paymentState.text = order.paymentState ? "Paid" : "Unpaied"
+        cell.totalLbl.text = "SAR \(order.total)"
         return cell
     }
     
@@ -61,14 +85,16 @@ extension AdminHome: UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
     }
+    
 }
 extension AdminHome: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        3
+        images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "serviceCell", for: indexPath) as! ServiceCell
+        cell.serviceImg.image = images[indexPath.row]
         
         return cell
     }
